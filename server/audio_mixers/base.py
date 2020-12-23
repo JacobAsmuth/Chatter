963546b04e403f8182e465a69fcd7104e7aa43c0
Jacob Asmuth <jacobasmuth@gmail.com>
2020-12-23 12:47:37 -0800

    last try on the import

--- server/audio_mixers/base.py
+++ server/audio_mixers/base.py
@@ -1,9 +1,9 @@
 import abc
 from server.client_object import ClientObject
-from server.settings import ServerSettings
+from server.settings import Settings
  
 class AudioMixerBase(abc.ABC):
 
     @abc.abstractstaticmethod
-    def mix(destination_client: ClientObject, all_voice_data: dict, settings: ServerSettings):
+    def mix(destination_client: ClientObject, all_voice_data: dict, settings: Settings):
         pass
