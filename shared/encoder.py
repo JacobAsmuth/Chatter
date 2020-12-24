9e6e2bb2e108162454028875fcff1c86bb80dd8b
Jacob Asmuth <jacobasmuth@gmail.com>
2020-12-23 23:25:08 -0800

    Added opus encoder

--- shared/encoder.py
+++ shared/encoder.py
@@ -1,10 +1,23 @@
+from pyogg.opus_encoder import OpusEncoder
+from pyogg.opus_decoder import OpusDecoder
+import shared.consts as consts
 
 class Encoder:
     def __init__(self) -> None:
-        pass
+        self._encoder = OpusEncoder()
+        self._decoder = OpusDecoder()
+
+        self._encoder.set_channels(consts.CHANNELS)
+        self._encoder.set_sampling_frequency(consts.SAMPLE_RATE)
+        self._encoder.set_application('voip')
+
+
+        self._decoder.set_channels(consts.CHANNELS)
+        self._decoder.set_sampling_frequency(consts.SAMPLE_RATE)
+
 
     def decode(self, frame: bytes) -> bytes:
-        return frame
+        return self._decoder.decode(frame)
 
     def encode(self, frame: bytes) -> bytes:
-        return frame
+        return self._encoder.encode(frame).tobytes()
